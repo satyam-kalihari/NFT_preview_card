0efba582687f2e05638fb0485ac44de2bbdc7252
satyam kalihari <satyamkalihari17@gmail.com>
2022-08-25 15:33:22 +0530

    clearing not needed code

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 let age = prompt("Enter your age", 17);
-if(age >= 18){
-    console.log("you can enjoy the site");
-}
-else{
-    console.log("sorry we cannot allow you to enter the site");
-}
 
 let friends = {
     "a" : 13,
