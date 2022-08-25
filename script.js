c9ef3e97d3287403f930d86b75fdbaa5a3b350c1
satyam kalihari <satyamkalihari17@gmail.com>
2022-08-25 13:03:14 +0530

    this is my NFT card preview project

---
+++ script.js
@@ -0,0 +1,17 @@
+let age = prompt("Enter your age", 17);
+if(age >= 18){
+    console.log("you can enjoy the site");
+}
+else{
+    console.log("sorry we cannot allow you to enter the site");
+}
+
+let friends = {
+    "a" : 13,
+    "b" : 24,
+    "c" : 10
+};
+
+for(myFriend in friends){
+    console.log(`It's ${myFriend} and it's age is ${friends[myFriends]}`);
+}
