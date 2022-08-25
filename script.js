d2ba5cd2bd737421ae402d242fcc3ee6159eb9ea
satyam kalihari <satyamkalihari17@gmail.com>
2022-08-25 18:28:42 +0530

    crealing

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// let age = prompt("Enter your age", 17);
-
-let friends = {
-    "a" : 13,
-    "b" : 24,
-    "c" : 10
-};
-
-for(myFriend in friends){
-    console.log(`It's ${myFriend} and it's age is ${friends[myFriend]}`);
-}
-
 //price icon interval
-
-let priceIcon = document.querySelectorAll('.price-icon:hover').setInterval(priceIcon, 1000);
-
-// setInterval(priceIcon, 1000);
